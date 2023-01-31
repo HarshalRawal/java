c55e65026283cad99c541bfe29118cf5db5a84cf
HarshalRawal <harshalrawal463@gmail.com>
2023-01-31 18:25:24 +0530

    change

--- demo.java
+++ demo.java
@@ -5,6 +5,7 @@ class demo
       int a = 10;
       int b;
       b = a++ + a++ + ++a + a-- + a++ + a-- + --a;
+      System.out.println("jdjf");
       System.out.println(a);
       System.out.println(b);
     }
