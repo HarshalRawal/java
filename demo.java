cd3133fdbcf5306210a5df5001b0266678f732fa
HarshalRawal <harshalrawal463@gmail.com>
2023-01-31 18:59:31 +0530

    changes

--- demo.java
+++ demo.java
@@ -3,10 +3,14 @@ class demo
     public static void main(String []args)
     {
       int a = 10;
-      int b;
-      b = a++ + a++ + ++a + a-- + a++ + a-- + --a;
-      System.out.println("jdjf");
-      System.out.println(a);
-      System.out.println(b);
+      int b = 20;
+      int c = 30;
+     if(a<b){
+      System.out.println(a<b && a<c && b<c );
+      System.out.println("hgg");
+     }
+     else{
+      System.out.println("jfkf");
+     }
     }
 }
